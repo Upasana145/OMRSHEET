65aa45fc0e298feff6029f65ed9779fea8d3a138
surajit.mitra <surajit.mitra@datacoresystems.co.in>
2024-09-24 22:03:14 +0530

    modifications in Kafka API2

--- backend/controllers/KafkaResultDataController.js
+++ backend/controllers/KafkaResultDataController.js
@@ -22,7 +22,7 @@ exports.getKafkaResults = async (req, res) => {
   const parts = splitString(key);
 
   // Accessing individual parts
-  const template_name = parts[0];
+  const t_name = parts[0];
   const batch_name = parts[1];
   const question_paper_name = parts[2];
 
@@ -30,8 +30,8 @@ exports.getKafkaResults = async (req, res) => {
   //Result Insertion Started
   try {
     const sql = `SELECT count(ID) as length FROM processed_omr_results WHERE t_name = ? AND batch_name = ? AND question_paper_name = ?`;
-    const result = await query({ query: sql, values: [template_name, batch_name, question_paper_name] });
-    console.log('template_name', template_name);
+    const result = await query({ query: sql, values: [t_name, batch_name, question_paper_name] });
+    console.log('t_name', t_name);
     console.log('batch_name', batch_name);
     console.log('question_paper_name', question_paper_name);
     
@@ -40,8 +40,8 @@ exports.getKafkaResults = async (req, res) => {
     }
 
     await query({
-      query: `UPDATE processed_omr_results SET result = ? WHERE template_name = ? AND batch_name ? AND question_paper_name = ?`,
-      values: [value, template_name, batch_name, question_paper_name]
+      query: `UPDATE processed_omr_results SET result = ? WHERE t_name = ? AND batch_name ? AND question_paper_name = ?`,
+      values: [value, t_name, batch_name, question_paper_name]
     });
     
     console.log("Inserted successfully");
