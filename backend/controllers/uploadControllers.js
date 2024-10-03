b16a754491ead8b351889fe9f958d1dc5dd9cd86
unknown <upasanabharti145@gmail.com>
2024-10-03 20:45:51 +0530

    frontend

--- backend/controllers/uploadControllers.js
+++ backend/controllers/uploadControllers.js
@@ -262,128 +262,6 @@ exports.uploadFile = async (req, res) => {
   }
 };
 
-exports.insertomrData = async (req, res) => {
-  const { template_name, map } = req.body;
-  console.log("hey i am template_name..", template_name, " ", map);
-
-  if ((template_name, map)) {
-    try {
-      let name_count = `select count(template_name) as count from save_data where template_name = '${template_name}'`;
-      const result1 = await query({
-        query: name_count,
-        values: [],
-      });
-      console.log("name_count:", result1);
-
-      console.log("name_count:", result1[0].count);
-
-      if (result1 && result1[0].count < 1) {
-        // Convert the map object to a JSON string
-        const mapString = JSON.stringify(map);
-        let sql = `INSERT INTO save_data( template_name,
-        map) VALUES ('${template_name}','${mapString}')`;
-
-        const result = await query({
-          query: sql,
-          values: [],
-        });
-
-        if (result) {
-          resSend(res, true, 200, "Dept list", result, null);
-        } else {
-          resSend(res, false, 200, "No Record Found!", result, null);
-        }
-      } else {
-        resSend(
-          res,
-          false,
-          200,
-          "Template name is already exists.",
-          null,
-          null
-        );
-      }
-    } catch (error) {
-      // Handle error
-      console.log(error);
-      resSend(res, false, 400, "Error", error, null);
-    }
-  } else {
-    resSend(res, false, 200, "Please fill all the inputs", null, null);
-  }
-};
-
-// POST/api/v1/master/editomrData
-exports.editomrData = async (req, res) => {
-  const { template_name, map } = req.body;
-  console.log("hey i am template " + template_name);
-  try {
-    let name_count = `SELECT COUNT(template_name) as count FROM save_data WHERE template_name  = '${template_name}'`;
-    const result2 = await query({
-      query: name_count,
-      values: [],
-    });
-
-    console.log("hey i am result2", result2, name_count);
-    console.log("hey i am result2", result2[0].count);
-
-    if (result2 && result2[0].count > 0) {
-      const mapString = JSON.stringify(map);
-      const currentDate = new Date().toISOString().slice(0, 10);
- 
-      let sql = `UPDATE save_data SET map=? WHERE template_name=?`;
-
-      const result = await query({
-        query: sql,
-        values: [mapString, template_name],
-      });
-
-      resSend(res, true, 200, "OMR Data Updated", result, null);
-    } else {
-      resSend(
-        res,
-        true,
-        200,
-        "you cann't edit because Template Name doesn't exists",
-        null,
-        null
-      );
-    }
-  } catch (error) {
-    console.log(error);
-    resSend(res, false, 400, "Error", error, null);
-  }
-};
-
-// POST/api/v1/master/deleteomrData
-exports.deleteomrData = async (req, res) => {
-  const { template_name } = req.body;
-  console.log("Hey i am template_name..", template_name);
-  let ngs_count = `SELECT COUNT(template_name) as count FROM save_data WHERE template_name = '${template_name}'`;
-  const result1 = await query({
-    query: ngs_count,
-    values: [],
-  });
-
-  // console.log("hey i am result: ", result1[0].count);
-
-  if (result1 && result1[0].count > 0) {
-    try {
-      // let sql = `Delete FROM save_data WHERE template_name = '${template_name}' AND is_deleted = 0 `;
-      let sql = `UPDATE save_data SET is_deleted = 1 WHERE template_name = '${template_name}' AND is_deleted = 0`;
-      const result = await query({
-        query: sql,
-        values: [],
-      });
-      resSend(res, true, 200, "OMR Data deleted successfully.", result, null);
-    } catch (error) {
-      resSend(res, false, 400, "Error", error, null);
-    }
-  } else {
-    resSend(res, false, 200, "Template name don't exist.", null, null);
-  }
-};
-
 
 exports.saveImgToDB = async (req, res) => {
   const { dept_name, camera, image, alarm_type } = req.body;
@@ -1060,7 +938,7 @@ exports.csvresult = async (req, res) => {
 
     // SQL query to select the correct results and batch names
     const selectJsonQuery = `
-      SELECT correct_result, batch_name 
+      SELECT correct_result, batch_name, question_paper_name 
       FROM processed_omr_results 
       WHERE t_name = ? AND flag = 1
     `;
